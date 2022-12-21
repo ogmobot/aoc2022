b28b4f08ff5dbacd1c9c77b637ce34d82b4de1fd
ogmobot <ogmobot@github.com>
2022-12-21 17:08:47 +1100

    Day 21 solution in Python (slightly washed)

---
+++ day21.py
@@ -0,0 +1,79 @@
+class Monkey:
+    def __init__(self, text, monkeys):
+        self.monkeys = monkeys
+        self.symbolic = False
+        if text.isnumeric():
+            self.is_atom = True
+            self.value = int(text)
+        else:
+            self.is_atom = False
+            words = text.split()
+            self.op = words[1]
+            self.a = words[0]
+            self.b = words[2]
+
+    def eval(self):
+        if self.is_atom:
+            if self.symbolic:
+                return "x"
+            return self.value
+        else:
+            a = self.monkeys[self.a].eval()
+            b = self.monkeys[self.b].eval()
+            return do_op(self.op, a, b)
+
+    def __str__(self):
+        return f"({self.op} {self.a} {self.b})"
+
+def do_op(op, a, b):
+    match op, a, b:
+        case "+", int(), int(): return a + b
+        case "-", int(), int(): return a - b
+        case "*", int(), int(): return a * b
+        case "/", int(), int(): return a // b
+        case "=", int(), int(): return a == b
+        case "=", int(), _:     return unwrap(a, b)
+        case "=", _,     int(): return unwrap(b, a)
+        case _,   int(), int(): raise ValueError(f"invalid op: '{op}'")
+        case _,   _,     _:     return [op, a, b]
+
+def do_reverse(op, result, b):
+    if op == "+":
+        return result - b
+    elif op == "-":
+        return result + b
+    elif op == "*":
+        return result // b
+    elif op == "/":
+        return result * b
+
+def unwrap(integer, expression):
+    while type(expression) == list:
+        op, a, b = expression
+        #print(f"{integer}")
+        #print(f"({op} {a} {b})")
+        if op == "-" and type(a) == int:
+            a, b = b, a
+            integer = -integer
+        if type(a) == int and type(b) != int:
+            integer = do_reverse(op, integer, a)
+            expression = b
+        elif type(b) == int and type(a) != int:
+            integer = do_reverse(op, integer, b)
+            expression = a
+    assert type(expression) == str, "Couldn't unwrap properly!"
+    return integer
+
+with open("input21.txt") as f:
+    lines = [line.strip() for line in f]
+
+monkeys = {}
+for line in lines:
+    parts = line.split(": ")
+    monkeys[parts[0]] = Monkey(parts[1], monkeys)
+
+print(monkeys["root"].eval())
+
+monkeys["root"].op = "="
+monkeys["humn"].symbolic = True
+print(monkeys["root"].eval())
