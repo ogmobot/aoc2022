a71acc7f2ba22a4ce145d304e4b186bec879f1a4
ogmobot <ogmobot@github.com>
2023-11-30 21:05:20 +1100

    Day 24 in F#

--- day24.fsx
+++ day24.fsx
@@ -1,9 +1,10 @@
 // Types
-type Coord     = { row: int; col: int }
-type TimeCoord = { r: int; c: int; t: int }
-type Blizzard  = { coord: Coord; facing: Coord }
+type Coord      = { row: int; col: int }
+type TimeCoord  = { r: int; c: int; t: int }
+type Blizzard   = { coord: Coord; facing: Coord }
 // Treat walls as blizzards with facing {0, 0}
-type Limits    = { minR: int; maxR: int; minC: int; maxC: int }
+type Limits     = { minR: int; maxR: int; minC: int; maxC: int }
+type Checkpoint = { target: Coord; printMe: bool }
 
 // Parsing
 let loadBlizzards lines =
@@ -27,29 +28,31 @@ let rec gcd a b =
 
 let simulateBlizzards blizzards maxRow maxCol =
     blizzards |> List.map (fun bliz ->
-        let newRow = (bliz.coord.row + maxRow + bliz.facing.row) % maxRow
-        let newCol = (bliz.coord.col + maxCol + bliz.facing.col) % maxCol
-        { coord = { row = newRow; col = newCol }; facing = bliz.facing })
+        match bliz with
+        | { coord = pos; facing = { row = 0; col = 0 } } -> bliz
+        | b ->
+            let newRow = (b.coord.row + maxRow + b.facing.row) % maxRow
+            let newCol = (b.coord.col + maxCol + b.facing.col) % maxCol
+            { coord = { row = newRow; col = newCol }; facing = b.facing })
 
 let calculateBlizzards blizzards maxRow maxCol =
-    let period = gcd maxRow maxCol
+    let period = maxRow * maxCol / (gcd maxRow maxCol)
     let mutable sim = blizzards
     let mutable res = Set.empty<TimeCoord>
     for t = 0 to period - 1 do
-        for b in blizzards do
+        for b in sim do
             res <- Set.add { r = b.coord.row; c = b.coord.col; t = t } res
         sim <- (simulateBlizzards sim maxRow maxCol)
     // returns a function
     fun tCoord ->
-        res |> Set.contains { tCoord with t = (tCoord.t % period) }
+        let tmp = res |> Set.contains { tCoord with t = (tCoord.t % period) }
+        tmp
 
 // Pathfinding
 
 let inBounds limits tc =
-    (tc.r >= limits.minR
-  && tc.r <  limits.maxR
-  && tc.c >= limits.minC
-  && tc.c <  limits.maxC)
+    ((tc.r >= limits.minR) && (tc.r < limits.maxR)
+  && (tc.c >= limits.minC) && (tc.c < limits.maxC))
 
 let adjacent obstacles limits tCoord =
     let newT = tCoord.t + 1
@@ -59,26 +62,31 @@ let adjacent obstacles limits tCoord =
         { tCoord with r = tCoord.r + 1; t = newT };
         { tCoord with c = tCoord.c - 1; t = newT };
         { tCoord with c = tCoord.c + 1; t = newT };
-    ] |> List.filter (fun tc -> (not (obstacles tc)) && (inBounds limits tc))
+    ] |> List.filter (fun tc -> not (obstacles tc))
+      |> List.filter (fun tc -> inBounds limits tc)
 
-let findPath obstacles (imCheckpoints: Coord list) limits =
-    let start = imCheckpoints.Head
-    let mutable checkpoints = imCheckpoints.Tail
-    let mutable fronts = 
-        adjacent obstacles limits { r = start.row; c = start.col; t = 0 }
+let solve obstacles (imCheckpoints: Checkpoint list) limits =
+    let mutable checkpoints = imCheckpoints
+    let mutable fronts =  [
+        { r = checkpoints.Head.target.row;
+          c = checkpoints.Head.target.col;
+          t = 0 }
+    ]
     let mutable visited = Set.empty<TimeCoord>
     while checkpoints.Length > 0 do
         let current = fronts.Head
-        printfn "%A" fronts
         fronts <- fronts.Tail
         if not (Set.contains current visited) then
             visited <- Set.add current visited
-            if { row = current.r; col = current.c } = checkpoints.Head then
+            let currCoord = { row = current.r; col = current.c }
+            if (currCoord = checkpoints.Head.target) then
+                if checkpoints.Head.printMe then
+                    printfn "%d" current.t
                 fronts <- []
                 checkpoints <- checkpoints.Tail
-                printfn "%d" current.t
             fronts <- List.concat [
-                fronts ; (adjacent obstacles limits current)]
+                fronts ; (adjacent obstacles limits current)
+            ]
 
 // Main
 let main () =
@@ -87,20 +95,19 @@ let main () =
     //     #.####
     // row0#....#
     //    1#....#
-    // Start position is (row = -1, col = 0)
-    // Goal position is (row = maxRow, col = maxCol - 1)
+
     let lines = List.ofSeq (System.IO.File.ReadLines "input24.txt")
     let maxRow = lines.Length - 2
     let maxCol = lines.Head.Length - 2
+    let start = { row = -1; col = 0 }
+    let goal = { row = maxRow; col = maxCol - 1 }
     let blizzards = loadBlizzards lines
     let blizzardNoJutsu = calculateBlizzards blizzards maxRow maxCol
-    findPath blizzardNoJutsu [
-        { row = -1; col = 0 };
-        { row = maxRow; col = maxCol + 1 }; // There ...
-        { row = -1; col = 0 }               // ... and back ...
-        { row = maxRow; col = maxCol + 1 }; // ... and back again
+    solve blizzardNoJutsu [
+        { target = start; printMe = false };
+        { target = goal;  printMe = true  };
+        { target = start; printMe = false };
+        { target = goal;  printMe = true  }
     ] { minR = -1; maxR = maxRow + 1; minC = 0; maxC = maxCol }
-    printfn "Done."
 
 main()
-// expected 232 715
